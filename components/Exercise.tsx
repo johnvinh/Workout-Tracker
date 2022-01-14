eb4ef668a3b69d44edc8a48feb2d564737896c5e
John Vinh <61337643+johnvinh@users.noreply.github.com>
2022-01-13 21:07:11 -0500

    changed styling of exercise titles

--- components/Exercise.tsx
+++ components/Exercise.tsx
@@ -1,5 +1,5 @@
 import {useState, useEffect} from 'react';
-import {View, Text, Route} from 'react-native';
+import {View, Text, Route, StyleSheet} from 'react-native';
 import 'react-native-get-random-values';
 import { v4 as uuidv4 } from 'uuid';
 import {NavigationProp} from '@react-navigation/native';
@@ -20,9 +20,17 @@ function Exercise(props: Props)
 
         return (
                 <View>
-                        <Text>{exercise}</Text>
+                        <Text style={styles.title} >{exercise}</Text>
                 </View>
         );
 }
+
+const styles = StyleSheet.create({
+        title: {
+                fontWeight: 'bold',
+                fontSize: 50,
+                textAlign: 'center'
+        }
+});
 
 export default Exercise;
