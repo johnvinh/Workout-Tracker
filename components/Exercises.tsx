3a774efb8b7db203f8dae32f2e2b21a5a70fb38f
John Vinh <61337643+johnvinh@users.noreply.github.com>
2022-01-13 19:26:39 -0500

    started working on exercises page

---
+++ components/Exercises.tsx
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+
+function Exercises()
+{
+        return (
+                <View>
+                        <Text>Test</Text>
+                </View>
+        );
+}
+
+export default Exercises;
